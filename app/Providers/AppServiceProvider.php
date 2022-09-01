6691fa88ecc3f106485d7dbbbdd656d4ea18f446
Josphat Kipyegon <josphatkips@gmail.com>
2022-09-01 10:31:27 +0300

    updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,10 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        // if($this->app->environment('production')) {
+        if($this->app->environment('production')) {
             URL::forceScheme('https');
 
             
-        // }
+        }
     }
 }
